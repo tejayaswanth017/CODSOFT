e40e6f41eaf0b2a932745c87092e8d0b0d772245
TEJA <tejayaswanth@gmail.com>
2026-06-18 13:29:37 +0530

    Add files via upload

---
+++ Task3-ImageCaptioning/image_caption.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+captions = {
+    "dog": "A dog is standing on the ground.",
+    "cat": "A cute cat is sitting.",
+    "car": "A car is parked on the road.",
+    "person": "A person is visible in the image.",
+    "cricket": "A cricket player is playing on the field.",
+    "tree": "A tree is present in the image."
+}
+
+image = Image.open("sample.jpg")
+
+print("Image loaded successfully!")
+print("Image size:", image.size)
+print("Image format:", image.format)
+
+keyword = input("Enter image type: ").lower()
+
+if keyword in captions:
+    print("Generated Caption:", captions[keyword])
+else:
+    print("Generated Caption: An object is present in the image.")
